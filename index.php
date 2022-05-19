dda6307c5a33a104d4cee0e34d39bef170985d14
Pc Yo <correo@pcyo.cl>
2022-05-18 22:20:15 -0400

    Nuevos archivos en rama develop

---
+++ index.php
@@ -0,0 +1,282 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>MTGroup</title>
+
+   <!-- swiper css link  -->
+   <link rel="stylesheet" href="https://unpkg.com/swiper@8/swiper-bundle.min.css" />
+
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="css/style.css">
+
+</head>
+
+<body>
+
+   <!-- header section starts  -->
+
+   <header class="header">
+
+      <section class="flex">
+
+         <a href="#inicio" class="logo">MTgroup.</a>
+
+         <nav class="navbar">
+            <a href="#inicio">Inicio</a>
+            <a href="#nosotros">Nosotros</a>
+            <a href="#contenido">Contenido</a>
+            <a href="#profesores">Profesores</a>
+            <a href="#login">Iniciar Sesión</a>
+         </nav>
+
+         <div id="menu-btn" class="fas fa-bars"></div>
+
+      </section>
+
+   </header>
+
+   <!-- header section ends -->
+
+   <!-- home section starts  -->
+
+   <section class="home" id="inicio">
+
+      <div class="row">
+
+         <div class="content">
+            <h3>Bienvenidos <span>Profesores de Música en Línea</span></h3>
+         </div>
+
+         <div class="image">
+            <img src="images/homg-img.svg" alt="">
+         </div>
+
+      </div>
+
+   </section>
+
+   <!-- home section ends -->
+
+   <!-- couter section stars  -->
+
+   <section class="about" id="nosotros">
+
+      <div class="row">
+
+         <div class="content">
+         <h1 class="heading">Nosotros</h1>
+            <p>Somos una página web para que niños y niñas aprendan con nuestros materiales educativos a través de videos con aprendizaje de instrumentos musicales. El fin es potenciar y despertar el interés de niños junto a sus padres para el servicio de clases.</p>
+         </div>
+
+      </div>
+
+   </section>
+
+   <section class="about">
+
+      <div class="row">
+
+         <div class="image">
+            <img src="images/about-img.svg" alt="">
+         </div>
+
+         <div class="content">
+            <h3>Canciones para niños</h3>
+            <p>Si deseas fomentar el aprendizaje de conceptos como las vocales o los números a través de las canciones o simplemente disfrutar de la música que escuchaste en la infancia, ¡has llegado al lugar ideal! Nuestras canciones están bellamente animadas e incluyen textos sincronizados con el fin de motivar a los niños hacia la lectura. Si te gustó la letra de la canción, te invitamos a descargarla haciendo clic en el botón de descarga. ¡A cantar se ha dicho!</p>
+         </div>
+
+      </div>
+
+   </section>
+
+   <!-- couter section ends -->
+
+   <!-- courses section starts  -->
+
+   <section class="courses" id="contenido">
+
+      <h1 class="heading">Contenido</h1>
+
+      <div class="swiper course-slider">
+
+         <div class="swiper-wrapper">
+
+            <div class="swiper-slide slide">
+               <img src="images/img_1.jpg" alt="">
+               <h3>Los cantos infantiles de Árbol ABC</h3>
+               <p>¿No te resulta increíble que aún puedes recordar las canciones que aprendiste en tu infancia? Existe una razón por la cual las canciones infantiles permanecen con nosotros; las canciones nos ayudan a aprender y recordar. Por esta razón, a pesar de los años y el paso de generaciones siguen siendo un instrumento educativo utilizado por los docentes alrededor del mundo.<br> Igualmente, las canciones para niños de preescolar y primaria contribuyen en la enseñanza del buen comportamiento y habilidades sociales. Muchos cantos infantiles también usan rimas para ayudar a los pequeños a aprender vocabulario y fortalecer las habilidades comunicativas.</p>
+            </div>
+
+            <div class="swiper-slide slide">
+               <img src="images/img_2.jpg" alt="">
+               
+               <h3>Canciones para niños de preescolar y primaria</h3>
+               <p>Al cantar una canción infantil, estrechamos las palabras lo cual permite a los pequeños identificar los sonidos y la formación de cada palabra. Según estudios, aquellos niños que aman la música y disfrutan de los cantos infantiles y la rima, aprenden a hablar a temprana edad y llegan a poseer un vocabulario más fluido lo cual facilita el aprendizaje, especialmente la lectura y la ortografía.</p>
+            </div>
+
+            <div class="swiper-slide slide">
+               <img src="images/img_3.jpg" alt="">
+               <h3>Beneficios de la música infantil</h3>
+               <p>El éxito académico no es el único beneficio de la música para los niños. La música infantil se extiende a todas las áreas del desarrollo infantil como la intelectual, social-emocional, motricidad y lenguaje. La música ayuda al cuerpo y la mente a trabajar juntos. Al bailar al ritmo de la música infantil, los niños desarrollan sus habilidades motoras mientras practican su autoexpresión. Sin embargo, el mayor beneficio de la música infantil es que proporciona alegría. ¡Piensa en tu infancia y verás que una canción infantil acompañará ese recuerdo!</p>
+            </div>
+
+            <div class="swiper-slide slide">
+               <img src="images/img_4.jpg" alt="">
+               <h3>El uso de subtítulos en los videos de canciones infantiles</h3>
+               <p>La presentación de la información de múltiples maneras puede ayudar a abordar las diversas necesidades de los niños en el hogar o en el aula y motivarlos a participar. El uso de subtítulos es una gran herramienta para los padres y maestros que buscan diferenciar la instrucción. Con nuestros videos de canciones infantiles, los pequeños no solo se divertirán escuchando la canción mientras observan las coloridas animaciones, sino también podrán reconocer las palabras que componen la canción a través de nuestros subtítulos sincronizados a manera de karaoke.</p>
+            </div>
+
+            <div class="swiper-slide slide">
+               <img src="images/img_5.jpg" alt="">
+               <h3>Letras de canciones infantiles</h3>
+               <p>En nuestro portal educativo incluimos las letras de las canciones infantiles para descargar e imprimir, así como actividades complementarias para el uso en casa o en el aula. Esperamos de todo corazón que tus pequeños se diviertan aprendiendo y disfrutando de las canciones infantiles tradicionales, así como nuestras letras de canciones infantiles originales. ¡No olvides que nuestra meta es educar y divertir!</p>
+            </div>
+
+         </div>
+
+         <div class="swiper-pagination"></div>
+
+      </div>
+
+   </section>
+
+   <!-- courses section ends -->
+
+   <!-- teachers section starts  -->
+
+   <section class="teachers" id="profesores">
+
+      <h1 class="heading">Profesores</h1>
+
+      <div class="swiper teachers-slider">
+
+         <div class="swiper-wrapper">
+
+            <div class="swiper-slide slide">
+               <img src="images/tutor-1.png" alt="">
+               <div class="share">
+                  <a href="#" class="fab fa-facebook-f"></a>
+                  <a href="#" class="fab fa-twitter"></a>
+                  <a href="#" class="fab fa-instagram"></a>
+                  <a href="#" class="fab fa-linkedin"></a>
+               </div>
+               <h3>john deo</h3>
+            </div>
+
+            <div class="swiper-slide slide">
+               <img src="images/tutor-2.png" alt="">
+               <div class="share">
+                  <a href="#" class="fab fa-facebook-f"></a>
+                  <a href="#" class="fab fa-twitter"></a>
+                  <a href="#" class="fab fa-instagram"></a>
+                  <a href="#" class="fab fa-linkedin"></a>
+               </div>
+               <h3>john deo</h3>
+            </div>
+
+            <div class="swiper-slide slide">
+               <img src="images/tutor-3.png" alt="">
+               <div class="share">
+                  <a href="#" class="fab fa-facebook-f"></a>
+                  <a href="#" class="fab fa-twitter"></a>
+                  <a href="#" class="fab fa-instagram"></a>
+                  <a href="#" class="fab fa-linkedin"></a>
+               </div>
+               <h3>john deo</h3>
+            </div>
+
+            <div class="swiper-slide slide">
+               <img src="images/tutor-4.png" alt="">
+               <div class="share">
+                  <a href="#" class="fab fa-facebook-f"></a>
+                  <a href="#" class="fab fa-twitter"></a>
+                  <a href="#" class="fab fa-instagram"></a>
+                  <a href="#" class="fab fa-linkedin"></a>
+               </div>
+               <h3>john deo</h3>
+            </div>
+
+            <div class="swiper-slide slide">
+               <img src="images/tutor-5.png" alt="">
+               <div class="share">
+                  <a href="#" class="fab fa-facebook-f"></a>
+                  <a href="#" class="fab fa-twitter"></a>
+                  <a href="#" class="fab fa-instagram"></a>
+                  <a href="#" class="fab fa-linkedin"></a>
+               </div>
+               <h3>john deo</h3>
+            </div>
+
+            <div class="swiper-slide slide">
+               <img src="images/tutor-6.png" alt="">
+               <div class="share">
+                  <a href="#" class="fab fa-facebook-f"></a>
+                  <a href="#" class="fab fa-twitter"></a>
+                  <a href="#" class="fab fa-instagram"></a>
+                  <a href="#" class="fab fa-linkedin"></a>
+               </div>
+               <h3>john deo</h3>
+            </div>
+
+         </div>
+
+         <div class="swiper-pagination"></div>
+
+      </div>
+
+   </section>
+
+   <!-- teachers section ends -->
+
+   <!-- footer section starts  -->
+
+   <footer class="footer">
+
+      <section>
+
+         <div class="share">
+            <a href="#" class="fab fa-facebook-f"></a>
+            <a href="#" class="fab fa-twitter"></a>
+            <a href="#" class="fab fa-linkedin"></a>
+            <a href="#" class="fab fa-instagram"></a>
+            <a href="#" class="fab fa-youtube"></a>
+         </div>
+
+         <div class="credit">&copy; copyright @ 2022 by <span>mr. web designer</span> | all rights reserved!</div>
+
+      </section>
+
+   </footer>
+
+   <!-- footer section ends -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   <!-- swiper js link  -->
+   <script src="https://unpkg.com/swiper@8/swiper-bundle.min.js"></script>
+
+   <!-- custom js file link  -->
+   <script src="js/script.js"></script>
+
+</body>
+
+</html>
